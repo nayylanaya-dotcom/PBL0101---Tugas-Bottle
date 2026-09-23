237b954c9d163374e6053392b328419121610911
Nayla Rahma A <naylarahmaku@gmail.com>
2026-09-23 11:10:40 +0700

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from bottle import route, run
+from datetime import datetime
 
 @route('/')
 def hello():
-    return 'HELLO WORLD'
+    waktu = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    return f'''
+    <h1>HELLO WORLD</h1>
+    <p>#PBW3B1PBL0101</p>
+    <p>251080200077</p>
+    <p>NAYLA_RAHMA_ALMUMIN</p>
+    <p>Framework Pilihan → Python [7] - Bottle</p>
+    <p>TIME : {waktu}</p>
+    '''
 
 run(host='localhost', port=8080, debug=True)
